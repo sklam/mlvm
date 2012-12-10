238c544909b79e6f6d037c3a27e93c3ea6e34eb5
Siu Kwan Lam <michael.lam.sk@gmail.com>
2012-12-09 21:56:20 -0600

    Refactor build_wrapper in LLVMBackend to a global function

--- mlvm/llvm/jit.py
+++ mlvm/llvm/jit.py
@@ -6,6 +6,9 @@ from ctypes import CFUNCTYPE, PYFUNCTYPE
 from llvm.ee import EngineBuilder
 from llvm.core import Module
 
+import logging
+
+logger = logging.getLogger(__name__)
 
 class LLVMExecutionManager(ExecutionManagerInterface):
     OPT_NONE = 0
@@ -32,7 +35,7 @@ class LLVMExecutionManager(ExecutionManagerInterface):
         k = (funcdef.name, tuple(funcdef.args))
         return self.__symlib[k]
 
-    def build_function(self, backend, funcdef, lfunc, attrs):
+    def build_function(self, backend, funcdef, lfunc, attrs, gil):
         '''Returns a wrapper and a ctype CFUNCTYPE.
 
             lfunc --- Ownership of lfunc is obtained.
@@ -47,36 +50,51 @@ class LLVMExecutionManager(ExecutionManagerInterface):
         # get function by name
         func = self.__fatmod.get_function_named(lfunc.name)
         # build wrapper
-        wrapper, callable = self.build_wrapper(backend, funcdef, func, attrs)
+        wrapper, callable = build_wrapper(self.__engine,
+                                          backend,
+                                          funcdef.return_type,
+                                          funcdef.args,
+                                          func,
+                                          gil)
 
         # remember JIT'ed functions
         self.__symlib[(funcdef.name, tuple(argtys))] = wrapper, callable
         return wrapper, callable
 
-    def build_wrapper(self, backend, funcdef, callee, gil=True):
-        # get address of funciton; forces JIT
-        address = self.__engine.get_pointer_to_function(callee)
-        # get ctypes of retty and argtys
-        get_ty_impl = backend.get_type_implementation
+def build_wrapper(engine, backend, return_type, args, callee, gil=True):
+    # get address of funciton; forces JIT
+    address = engine.get_pointer_to_function(callee)
+    # get ctypes of retty and argtys
+    get_ty_impl = backend.get_type_implementation
 
-        rettyimpl = get_ty_impl(funcdef.return_type)
-        argtyimpls = [get_ty_impl(x) for x in funcdef.args]
-        
-        cretty = rettyimpl.ctype(backend)
-        cargtys = [x.ctype(backend) for x in argtyimpls]
+    rettyimpl = get_ty_impl(return_type)
+    argtyimpls = [get_ty_impl(x) for x in args]
+    
+    cretty = rettyimpl.ctype(backend)
+    cargtys = [x.ctype(backend) for x in argtyimpls]
 
-        if gil:
-            ffi = PYFUNCTYPE
-        else:
-            ffi = CFUNCTYPE
+    if gil:
+        ffi = PYFUNCTYPE
+    else:
+        ffi = CFUNCTYPE
 
-        callable = ffi(cretty, *cargtys)(address)
+    callable = ffi(cretty, *cargtys)(address)
 
-        def _wrapper(*args):
-            args = [ty.ctype_argument(backend, arg)
-                    for ty, arg in zip(argtyimpls, args)]
-            retval = callable(*args)
-            return rettyimpl.ctype_return(backend, retval)
+    def _wrapper(*args):
+        actual_args = []
+        if len(argtyimpls) != len(args):
+            raise TypeError("Function takes exactly %d arguments; but got %d" %\
+                            (len(argtyimpls), len(args)))
+        for i, (ty, arg) in enumerate(zip(argtyimpls, args)):
+            try:
+                prepared = ty.ctype_argument(backend, arg)
+            except:
+                logger.debug("Error at argument %d: %s %s", i, ty, arg)
+                raise
+            actual_args.append(prepared)
+        retval = callable(*actual_args)
+        return rettyimpl.ctype_return(backend, retval)
 
-        return _wrapper, callable
+
+    return _wrapper, callable
 
